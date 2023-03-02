e1128369f75539acdcbe3f44de0a0e1a51407cee
Shakil <shakilpp123456@gmail.com>
2023-03-03 03:49:29 +0530

    file updated

--- routes/Block.route.js
+++ routes/Block.route.js
@@ -6,9 +6,6 @@ const { BlockModel } = require("../model/block.model");
 const BlockRouter = express.Router();
 
 //all users
-
-
-
 BlockRouter.get("/", async (req, res) => {
 
   const notes = await BlockModel.find()
@@ -28,27 +25,15 @@ BlockRouter.post("/register", async (req, res) => {
       await user.save();
       res.send({ massege: "New user has been ban" });
     }
-
-
-
-
-
   } catch (error) {
     res.send({ massege: "something went wrong" });
   }
-
-
 });
-
-
-
-
 
 BlockRouter.delete("/delete/:id", async (req, res) => {
   const id = req.params.id
   await BlockModel.findByIdAndDelete({ _id: id })
   res.send({ massege: `Product ${id} has been been remove from ban list.` });
-
 });
 
 module.exports = {
--- routes/Cart.route.js
+++ routes/Cart.route.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const { CartModel } = require("../model/cart.model");
