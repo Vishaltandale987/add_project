4442c2ad72cc51918242e393ac813d9b30b5c486
Shakil <shakilpp123456@gmail.com>
2023-03-03 06:12:29 +0530

    file updated

--- index.js
+++ index.js
@@ -13,9 +13,16 @@ require("dotenv").config()
 
 const app=express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 //get
